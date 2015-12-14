ad5461374c2c74ca9234f355eb7b47a6a56552f8
sandhyasanju <p.sandhyarani20@gmail.com>
2015-12-14 19:09:47 +0530

    changes

--- 2048game/2048.js
+++ 2048game/2048.js
@@ -1,6 +1,7 @@
-var number = Math.floor(Math.random()*(3 - 0 + 1))+0;
-var number2 = Math.floor(Math.random()*(3 - 0 + 1))+0;
+var leftKey , rightKey , upKey , downKey;
 $(document).ready(function() {
+  var number = Math.floor(Math.random()*(3 - 0 + 1))+0;
+  var number2 = Math.floor(Math.random()*(3 - 0 + 1))+0;
   var table = $("<table class='table table--bordered'></table>");
   for ( var row = 1 ; row < 5 ; row++ ) {
     var tr = $("<tr class='table1'></tr>");
@@ -13,6 +14,15 @@ $(document).ready(function() {
     $("table").find("tr").eq(number2).find("td").eq(number).html(2);
     $("table").find("tr").eq(number).find("td").eq(number2).html(2);
   }
+  var endGameDiv = $("<div></div>");
+  $(endGameDiv).attr("class" , "transparent");
+  var button = "<button class='btn btn--tertiaery'>Try Again</button>";
+  $(button).attr("id","button");
+  endGameDiv.html("End Game");
+  endGameDiv.append(button);
+  endGameDiv.hide();
+  $("#body").append(endGameDiv);
+  $("body").append($("#body"));
 });
 $(document).keydown(function(event) {
   if (event.keyCode == 37) {
@@ -25,144 +35,177 @@ $(document).keydown(function(event) {
           down();
         }
 });
-function left(){
+function left() {
+  var added , moved;
   for ( var rows = 0 ; rows <= ($("table").find("tr").length)-1 ; rows++ ) {
     for ( var cell = 0 ; cell <= ($("table").find("tr").eq(0).find("td").length)-1 ; cell++ ) {
+      var temp = cell;
       var leftValue1 = $("table").find("tr").eq(rows).find("td").eq(cell).html();
-        if ( leftValue1 === "" ) {
-          for ( var nextCell = cell+1 ; nextCell <= ($("table").find("tr").eq(0).find("td").length)-1 ; nextCell++ ) {
-            var rightValue1 = $("table").find("tr").eq(rows).find("td").eq(nextCell).html();
-            if ( rightValue1 !== "") {
-              $("table").find("tr").eq(rows).find("td").eq(nextCell).html("");
-              $("table").find("tr").eq(rows).find("td").eq(cell).html(rightValue1);
-              if(nextCell===cell+1)
-              break;
+      //alert(leftValue1);
+      if ( leftValue1 === "" ) {
+        for ( var nextCell = temp+1 ; nextCell <= ($("table").find("tr").eq(0).find("td").length)-1 ; nextCell++ ) {
+          var rightValue1 = $("table").find("tr").eq(rows).find("td").eq(nextCell).html();
+          if ( rightValue1 !== "") {
+            $("table").find("tr").eq(rows).find("td").eq(cell).html(rightValue1);
+            $("table").find("tr").eq(rows).find("td").eq(nextCell).html("");
+            moved = true;
+          }
+          temp++;
+        }
+      } else {
+          for ( var nextCells = temp+1 ; nextCells <= ($("table").find("tr").eq(0).find("td").length)-1 ; nextCells++ ) {
+            var rightValue = ($("table").find("tr").eq(rows).find("td").eq(nextCells).html());
+            if ( rightValue !== "" ) {
+              if ( leftValue1 === rightValue ) {
+                $("table").find("tr").eq(rows).find("td").eq(cell).html(parseInt(leftValue1)+parseInt(rightValue));
+                $("table").find("tr").eq(rows).find("td").eq(cell+1).html("");
+                added = true;
+              }
             }
+            temp++;
           }
         }
-    }
-    for ( var cells = 0 ; cells <= ($("table").find("tr").eq(0).find("td").length)-1 ; cells++ ) {
-      var leftValue = parseInt($("table").find("tr").eq(rows).find("td").eq(cells).html());
-      var temp = rows;
-      if ( leftValue !== "" ) {
-        for ( var nextCells = cells+1 ; nextCells <= ($("table").find("tr").eq(0).find("td").length)-1 ; nextCells++ ) {
-          var rightValue = parseInt($("table").find("tr").eq(rows).find("td").eq(nextCells).html());
-          if ( rightValue !== "" ) {
-            if ( leftValue === rightValue ) {
-              $("table").find("tr").eq(rows).find("td").eq(nextCells).html("");
-              $("table").find("tr").eq(rows).find("td").eq(cells).html(leftValue+rightValue);
-            }
-          }
-        }
-      }
     }
   }
-  randomNumber();
+  if ( added === true || moved === true ) {
+    randomNumber();
+    leftKey = true;
+    endGame();
+  }
 }
 function up() {
+  var added , moved;
   for ( var cells = 0 ; cells <= $("table").find("tr").eq(0).find("td").length-1 ; cells++ ) {
     for ( var row = 0 ; row <= ($("table").find("tr").length)-1 ; row++ ) {
+      var temp1 = row+1;
       var upperValue1 = $("table").find("tr").eq(row).find("td").eq(cells).html();
       if ( upperValue1 === "" ) {
-        for ( var nextRow = row+1 ; nextRow <= ($("table").find("tr").length)-1 ; nextRow++ ) {
-          var lowerValue1 = $("table").find("tr").eq(nextRow).find("td").eq(cells).html();
+        for ( var nextRow = temp1+1 ; nextRow <= ($("table").find("tr").length)-1 ; nextRow++ ) {
+          var lowerValue1 = $("table").find("tr").eq(temp1).find("td").eq(cells).html();
           if ( lowerValue1 !== "") {
             $("table").find("tr").eq(row).find("td").eq(cells).html(lowerValue1);
-            $("table").find("tr").eq(nextRow).find("td").eq(cells).html("");
-            if(nextRow===row+1)
-            break;
+            $("table").find("tr").eq(temp1).find("td").eq(cells).html("");
+            moveed = true;
           }
+          temp1++;
         }
-      }
-    }
-    for ( var rows = 0 ; rows <= $("table").find("tr").length-1 ; rows++ ) {
-      var upperValue = parseInt($("table").find("tr").eq(rows).find("td").eq(cells).html());
-      var temp = rows;
-      if ( upperValue !== "" ) {
-        for ( var nextRows = rows+1 ; nextRows <= ($("table").find("tr").length)-1 ; nextRows++ ) {
-          var lowerValue = parseInt($("table").find("tr").eq(nextRows).find("td").eq(cells).html());
+      }else {
+        for ( var nextRows = temp1 ; nextRows <= ($("table").find("tr").length)-1 ; nextRows++ ) {
+          var lowerValue = ($("table").find("tr").eq(temp1).find("td").eq(cells).html());
           if ( lowerValue !== "" ) {
-            if ( upperValue === lowerValue ) {
-              $("table").find("tr").eq(nextRows).find("td").eq(cells).html("");
-              $("table").find("tr").eq(rows).find("td").eq(cells).html(upperValue+lowerValue);
+            if ( upperValue1 === lowerValue ) {
+              $("table").find("tr").eq(row).find("td").eq(cells).html(parseInt(upperValue1)+parseInt(lowerValue));
+              $("table").find("tr").eq(temp1).find("td").eq(cells).html("");
+              added = true;
             }
           }
+          temp1++;
+        }
         }
       }
     }
+
+  if ( added === true || moved === true ) {
+    randomNumber();
+    upKey = true;
   }
-  randomNumber();
 }
 function right() {
+  var moved , added;
   for ( var rows = 0 ; rows <= ($("table").find("tr").length)-1 ; rows++ ) {
-    for ( var cells = ($("table").find("tr").eq(0).find("td").length)-1 ; cells >= 0  ; cells-- ) {
-      var rightValue = parseInt($("table").find("tr").eq(rows).find("td").eq(cells).html());
-      if ( rightValue !== "" ) {
-        for ( var previousCells = cells-1 ; previousCells >= 0 ; previousCells-- ) {
-          var leftValue = parseInt($("table").find("tr").eq(rows).find("td").eq(previousCells).html());
-          if ( leftValue !== "" ) {
-            if ( rightValue === leftValue ) {
-              $("table").find("tr").eq(rows).find("td").eq(previousCells).html("");
-              $("table").find("tr").eq(rows).find("td").eq(cells).html(leftValue+rightValue);
+    for ( var cell = ($("table").find("tr").eq(0).find("td").length)-1 ; cell >=0  ; cell-- ) {
+      var temp = cell-1;
+      var rightValue = $("table").find("tr").eq(rows).find("td").eq(cell).html();
+      if ( rightValue === "" ) {
+        for ( var previousCell = temp-1 ; previousCell >= 0 ; previousCell-- ) {
+          var leftValue1 = $("table").find("tr").eq(rows).find("td").eq(temp).html();// alert(temp);
+          if ( leftValue1 !== "") {
+            $("table").find("tr").eq(rows).find("td").eq(cell).html(leftValue1);
+            $("table").find("tr").eq(rows).find("td").eq(temp).html("");
+            moved = true;
+          }
+         temp--;
+
+        }
+      } else {
+        for ( var previousCells = temp-1 ; previousCells >= 0 ; previousCells-- ) {
+            var leftValue =($("table").find("tr").eq(rows).find("td").eq(temp).html());
+            if ( leftValue !== "" ) {
+              if ( rightValue === leftValue ) {
+                $("table").find("tr").eq(rows).find("td").eq(cell).html( parseInt(leftValue)+parseInt(rightValue));
+                $("table").find("tr").eq(rows).find("td").eq(temp).html("");
+                added = true;
+              }
+            }
+            temp--;
+          }
+      }
+    }
+
+  }
+  if ( moved === true || added === true ) {
+    randomNumber();
+    rightKey = true;
+  }
+}
+function down() {
+  var added , moved;
+  for ( var cells = 0 ; cells <= ($("table").find("tr").eq(0).find("td").length)-1 ; cells++ ) {
+    for ( var row = ($("table").find("tr").length)-1 ; row >= 0  ; row-- ) {
+      var temp1 = row;
+      var lowerValue1 = $("table").find("tr").eq(row).find("td").eq(cells).html();
+      if ( lowerValue1 === "" ) {
+        for ( var previousRow = temp1-1 ; previousRow >= 0 ; previousRow-- ) {
+          var upperValue1 = $("table").find("tr").eq(previousRow).find("td").eq(cells).html();
+          if ( upperValue1 !== "") {
+            $("table").find("tr").eq(row).find("td").eq(cells).html(upperValue1);
+            $("table").find("tr").eq(previousRow).find("td").eq(cells).html("");
+            moved = true;
+          }
+          temp1--;
+        }
+      } else {
+          for ( var previousRows = temp1-1 ; previousRows >= 0 ; previousRows-- ) {
+            var upperValue = parseInt($("table").find("tr").eq(row-1).find("td").eq(cells).html());
+            if ( upperValue !== "" ) {
+              if ( lowerValue1 == upperValue ) {
+                $("table").find("tr").eq(row).find("td").eq(cells).html(upperValue+parseInt(lowerValue1));
+                $("table").find("tr").eq(row-1).find("td").eq(cells).html("");
+                added = true;
+              }
+              temp1--;
             }
           }
         }
       }
-    }
-    for ( var cell = ($("table").find("tr").eq(0).find("td").length)-1 ; cell >=0  ; cell-- ) {
-      var rightValue1 = $("table").find("tr").eq(rows).find("td").eq(cell).html();
-      if ( rightValue1 === "" ) {
-        for ( var previousCell = cell-1 ; previousCell >= 0 ; previousCell-- ) {
-          var leftValue1 = $("table").find("tr").eq(rows).find("td").eq(previousCell).html();
-          if ( leftValue1 !== "") {
-            $("table").find("tr").eq(rows).find("td").eq(previousCell).html("");
-            $("table").find("tr").eq(rows).find("td").eq(cell).html(leftValue1);
-            if(previousCell===cell-1)
-            break;
-          }
-        }
-      }
-    }
   }
-  randomNumber();
-}
-function down() {
-  for ( var cells = 0 ; cells <= ($("table").find("tr").eq(0).find("td").length)-1 ; cells++ ) {
-  for ( var rows = ($("table").find("tr").length)-1 ; rows >= 0 ; rows-- ) {
-    var lowerValue = parseInt($("table").find("tr").eq(rows).find("td").eq(cells).html());
-    if ( lowerValue !== "" ) {
-      for ( var previousRows = rows-1 ; previousRows >= 0 ; previousRows-- ) {
-        var upperValue = parseInt($("table").find("tr").eq(previousRows).find("td").eq(cells).html());
-        if ( upperValue !== "" ) {
-          if ( lowerValue === upperValue ) {
-            $("table").find("tr").eq(previousRows).find("td").eq(cells).html("");
-            $("table").find("tr").eq(rows).find("td").eq(cells).html(upperValue+lowerValue);
-          }
-        }
-      }
-    }
+  if ( moved === true || added === true ) {
+    randomNumber();
+    downKey = true;
   }
-for ( var row = ($("table").find("tr").length)-1 ; row >= 0  ; row-- ) {
-    var lowerrValue1 = $("table").find("tr").eq(row).find("td").eq(cells).html();
-    if ( lowerrValue1 === "" ) {
-      for ( var previousRow = row-1 ; previousRow >= 0 ; previousRow-- ) {
-        var upperValue1 = $("table").find("tr").eq(previousRow).find("td").eq(cells).html();
-        if ( upperValue1 !== "") {
-          $("table").find("tr").eq(previousRow).find("td").eq(cells).html("");
-          $("table").find("tr").eq(row).find("td").eq(cells).html(upperValue1);
-          if(previousRow===row-1)
-          break;
-        }
-      }
-    }
-  }
-}
-randomNumber();
 }
 function randomNumber() {
-  if($("table").find("tr").eq(number).find("td").eq(number2).html() === "") {
-    $("table").find("tr").eq(number).find("td").eq(number2).html(2);
+  var number1 = Math.floor(Math.random()*(3 - 0 + 1))+0;
+  var number3 = Math.floor(Math.random()*(3 - 0 + 1))+0;
+  if ( $("table").find("tr").eq(number1).find("td").eq(number3).html() === "" ) {
+      $("table").find("tr").eq(number1).find("td").eq(number3).html(2);
   } else {
       randomNumber();
     }
+}
+function endGame() {
+  if(leftKey !== true && downKey !== true && rightKey !== true && upKey !== true) {
+    $(".table").append($(".transparent").slideDown("slow"));
+    $(".btn").click(function() {
+      newGame();
+    });
+  }
+}
+function newGame() {
+  var number1 = Math.floor(Math.random()*(3 - 0 + 1))+0;
+  var number3 = Math.floor(Math.random()*(3 - 0 + 1))+0;
+  $("table").find("td").html("");
+  $(".transparent").slideUp();
+  $("table").find("tr").eq(number1).find("td").eq(number3).html(2);
+  $("table").find("tr").eq(number3).find("td").eq(number1).html(2);
 }
